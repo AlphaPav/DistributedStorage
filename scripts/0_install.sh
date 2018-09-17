d0b632831f0dc7210373a228e84ae58a11381ab8
Liyi Zhou <liyizhou@Liyis-MacBook-Pro.local>
2018-09-18 01:17:07 +0800

    amended ipfs to latest

--- scripts/0_install.sh
+++ scripts/0_install.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
-# Install ipfs
-cd $HOME
-wget https://dist.ipfs.io/go-ipfs/v0.4.17/go-ipfs_v0.4.17_linux-amd64.tar.gz
-tar xf go-ipfs_v0.4.17_linux-amd64.tar.gz
-sudo mv go-ipfs/ipfs /usr/local/bin/ipfs
-ipfs init
-
 # Installing go
-
 cd $HOME/ && wget https://dl.google.com/go/go1.11.linux-amd64.tar.gz
 tar -xvf go1.11.linux-amd64.tar.gz
 mkdir $HOME/gopath
@@ -16,7 +8,12 @@ echo "export GOROOT=\$HOME/go" >> .bashrc
 echo "export PATH=\$PATH:\$GOROOT/bin" >> .bashrc
 source ~/.bashrc
 
+# Install IPFS
+go get -u github.com/ipfs/ipfs-update
+ipfs-update install latest
+ipfs init
+
 # Initialize ipfs-cluster
-go get -u -d github.com/ipfs/ipfs-cluster
+git clone https://github.com/ipfs/ipfs-cluster.git $GOPATH/src/github.com/ipfs/ipfs-cluster
 cd $GOPATH/src/github.com/ipfs/ipfs-cluster
 make install
