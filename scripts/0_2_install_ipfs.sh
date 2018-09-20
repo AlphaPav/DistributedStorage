00ae5b3f57985568de6f81f5c78797983ab9ed8b
Liyi Zhou <liyizhou@Liyis-MacBook-Pro.local>
2018-09-20 16:01:45 +0800

    modified to isntall ipfs using go-ipfs

--- scripts/0_2_install_ipfs.sh
+++ scripts/0_2_install_ipfs.sh
@@ -2,7 +2,11 @@
 
 # Install IPFS
 echo "--- Start installing IPFS ---"
-go get -u github.com/ipfs/ipfs-update
-ipfs-update install latest
+
+go get -u -d github.com/ipfs/go-ipfs
+cd $GOPATH/src/github.com/ipfs/go-ipfs
+make install
+# go get -u github.com/ipfs/ipfs-update
+# ipfs-update install latest
 ipfs init
 echo "--- Finished installing IPFS ---"
