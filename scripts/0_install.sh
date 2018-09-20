13436ab25600e088d86d80e4effc2afd1b125c2a
EC2 Default User <ec2-user@ip-172-31-17-213.ap-northeast-2.compute.internal>
2018-09-20 07:08:58 +0000

    added go root bin to file

--- scripts/0_install.sh
+++ scripts/0_install.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 # Installing go
+echo "--- Start Installing Go ---"
 cd $HOME/ && wget https://dl.google.com/go/go1.11.linux-amd64.tar.gz
 tar -xvf go1.11.linux-amd64.tar.gz
 mkdir -p $HOME/gopath
 echo "export GOPATH=\$HOME/gopath" >> .bash_profile
 echo "export GOROOT=\$HOME/go" >> .bash_profile
+echo "export PATH=\$PATH:\$GOROOT/bin" >> .bash_profile
 echo "export PATH=\$PATH:\$GOPATH/bin" >> .bash_profile
 source ~/.bash_profile
 #export PATH=$PATH:$(go env GOPATH)/bin
 
 # Install IPFS
+echo "--- Start installing IPFS ---"
 go get -u github.com/ipfs/ipfs-update
 ipfs-update install latest
 ipfs init
 
 # Initialize ipfs-cluster
+echo "--- Start installing ipfs cluster"
 git clone https://github.com/ipfs/ipfs-cluster.git $GOPATH/src/github.com/ipfs/ipfs-cluster
 cd $GOPATH/src/github.com/ipfs/ipfs-cluster
 make install
